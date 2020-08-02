61f85ed47da70339daba7da5946db8f50d626538
Mryan2005 <40586009+Mryan2005@users.noreply.github.com>
2020-08-02 02:35:33 -0500

    Create mian.py

---
+++ mian.py
@@ -0,0 +1,23 @@
+def replace(mumber):
+  try:
+    if int(mumber) == 1:
+      word = "a"
+    if int(mumber) == 2:
+      word = "b"
+    if int(mumber) == 3:
+      word = "c"
+    if int(mumber) == 4:
+      word = "d"
+    if int(mumber) == 5:
+      word = "e"
+    if int(mumber) == 6:
+      word = "f"
+    if int(mumber) == 7:
+      word = "g"
+    if int(mumber) == 8:
+      word = "h"
+    if int(mumber) == 9:
+      word = "i"
+    if int(mumber) == 0:
+      word = "j"
+    return word
